5b9d503ca3c29847bd70ba01d5b6bfc59ffe1665
youness boufous <younesss.bfs@gmail.com>
2018-02-16 15:42:26 +0000

    Add some Test

---
+++ src/AppBundle/Repository/UserRepository.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: youness
+ * Date: 2018-02-16
+ * Time: 10:53 AM
+ */
+
+namespace AppBundle\Repository;
+
+
+use Doctrine\ORM\EntityRepository;
+
+class UserRepository extends EntityRepository
+{
+
+}
